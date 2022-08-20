acbb8cda324ce7a9aead1043efb3b832283c7e2e
Mark Whitehead <mjamesw2010@gmail.com>
2022-08-20 17:31:54 +0100

    created a function so add a setSail method to ship

--- src/Ship.js
+++ src/Ship.js
@@ -1,7 +1,16 @@
 class Ship {
   constructor(name) {
-    this.startingPort = "Dover";
+    this.name = name;
+    this.startingPort = 0;
   }
 }
+
+// If the ship doesnt have a name then it should return a falsy value.applythe ship .setsail function should push a name to the ship and then return a  truthy value. it needs to check if this is the case
+// If a new ship hasnt been created using the constructor
+//   ship .set sail should create a new ship and pass a starting port as a paramater
+
+Ship.prototype.setSail = function (startingPort) {
+  this.startingPort = startingPort;
+};
 
 module.exports = Ship;
--- src/__tests__/Ship.test.js
+++ src/__tests__/Ship.test.js
@@ -7,8 +7,19 @@ describe("Ship", () => {
     expect(new Ship()).toBeInstanceOf(Object);
   });
 
+  it("has a name", () => {
+    const ship = new Ship("Alliance");
+    expect(ship.name).toEqual("Alliance");
+  });
+
   it("has a starting port", () => {
-    const ship = new Ship("Dover");
-    expect(ship.startingPort).toBe("Dover");
+    const ship = new Ship("Alliance");
+    expect(ship.startingPort).toEqual(0);
+  });
+
+  it("sets sail", () => {
+    const ship = new Ship("Alliance");
+    ship.setSail();
+    expect(Ship.startingPort).toBeFalsy();
   });
 });
