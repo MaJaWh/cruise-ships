3b468c852ded6921eb6160c5310aed59419784fb
Mark Whitehead <mjamesw2010@gmail.com>
2022-08-29 21:50:31 +0100

    Initialised cruise ships project

--- src/__tests__/Ship.test.js
+++ src/__tests__/Ship.test.js
@@ -6,14 +6,27 @@ const Itinerary = require("../Itinerary");
 
 describe("Ship", () => {
   describe("with ports and an itinerary", () => {
-    let ship;
     let dover;
     let calais;
-    let itinerary;
 
     beforeEach(() => {
-      dover = new Port("Dover");
-      calais = new Port("calais");
+      dover = {
+        addShip: jest.fn(),
+        removeShip: jest.fn(),
+        name: "Dover",
+        ships: [],
+      };
+
+      calais = {
+        addShip: jest.fn(),
+        removeShip: jest.fn(),
+        name: "calais",
+        ships: [],
+      };
+      itinerary = {
+        ports: [dover, calais],
+      };
+
       itinerary = new Itinerary([dover, calais]);
       ship = new Ship("Alliance", itinerary);
     });
@@ -34,59 +47,58 @@ describe("Ship", () => {
       ship.setSail();
 
       expect(Ship.currentPort).toBeFalsy();
-      expect(dover.ships).not.toContain(ship);
+      expect(dover.removeShip).toHaveBeenCalledWith(ship);
     });
 
     it("gets added to port on instantiation", () => {
-      expect(dover.ships).toContain(ship);
+      expect(dover.addShip).toHaveBeenCalledWith(ship);
     });
-  });
+    it("can dock at a different port", () => {
+      const dover = new Port("Dover");
+      const calais = new Port("calais");
+      const itinerary = new Itinerary([dover, calais]);
+      const ship = new Ship("Alliance", itinerary);
 
-  it("can dock at a different port", () => {
-    const dover = new Port("Dover");
-    const calais = new Port("calais");
-    const itinerary = new Itinerary([dover, calais]);
-    const ship = new Ship("Alliance", itinerary);
+      // console.log(ship);
+      ship.setSail();
+      ship.dock();
 
-    // console.log(ship);
-    ship.setSail();
-    ship.dock();
+      expect(ship.currentPort).toBe(calais);
+      expect(calais.ships).toContain(ship);
+    });
 
-    expect(ship.currentPort).toBe(calais);
-    expect(calais.ships).toContain(ship);
-  });
+    it("cant't sail further than its itinerary", () => {
+      const dover = new Port("Dover");
+      const calais = new Port("calais");
+      const itinerary = new Itinerary([dover, calais]);
+      const ship = new Ship("Alliance", itinerary);
 
-  it("cant't sail further than its itinerary", () => {
-    const dover = new Port("Dover");
-    const calais = new Port("calais");
-    const itinerary = new Itinerary([dover, calais]);
-    const ship = new Ship("Alliance", itinerary);
+      ship.setSail();
+      ship.dock();
 
-    ship.setSail();
-    ship.dock();
+      expect(() => ship.setSail()).toThrowError("End of itinerary reached");
+    });
+    it("can add a ship", () => {
+      const port = new Port("Dover");
+      const ship = jest.fn();
 
-    expect(() => ship.setSail()).toThrowError("End of itinerary reached");
-  });
-  it("can add a ship", () => {
-    const port = new Port("Dover");
-    const ship = jest.fn();
+      port.addShip(ship);
 
-    port.addShip(ship);
+      expect(port.ships).toContain(ship);
+    });
 
-    expect(port.ships).toContain(ship);
-  });
+    it("can remove a ship", () => {
+      const port = new Port("Dover");
+      const titanic = jest.fn();
+      const queenMary = jest.fn();
 
-  it("can remove a ship", () => {
-    const port = new Port("Dover");
-    const titanic = jest.fn();
-    const queenMary = jest.fn();
+      port.addShip(titanic);
+      port.addShip(queenMary);
 
-    port.addShip(titanic);
-    port.addShip(queenMary);
-    console.log(port);
-    port.removeShip(queenMary);
-    console.log(port);
+      port.removeShip(queenMary);
+      console.log(port);
 
-    expect(port.ships).toEqual([titanic]);
+      expect(port.ships).toEqual([titanic]);
+    });
   });
 });
